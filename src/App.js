38e3a13c6ee298e2062f3eb74db0ba445d751f0a
Jahid <jahidulhaque767@gmail.com>
2021-11-03 04:56:17 +0600

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home/Home/Home";
 
 function App() {
   return (
     <div>
-      <h2>This is app</h2>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
