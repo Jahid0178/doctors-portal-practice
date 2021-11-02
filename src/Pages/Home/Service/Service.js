b0d6939cd2696447e00fb34d3481e2e8cf12db7e
Jahid <jahidulhaque767@gmail.com>
2021-11-03 05:58:45 +0600

    added some pages and complete service we provide

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { CardMedia } from "@mui/material";
+
+const Service = ({ service }) => {
+  const { img, name, description } = service;
+  return (
+    <>
+      <Grid item xs={2} sm={4} md={4}>
+        <Card
+          sx={{
+            minWidth: 275,
+            mt: "3rem",
+            textAlign: "center",
+            boxShadow: 0,
+          }}
+        >
+          <CardMedia
+            component="img"
+            sx={{ width: "auto", height: 80, margin: "0 auto" }}
+            image={img}
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {name}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {description}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </>
+  );
+};
+
+export default Service;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,10 +1,60 @@
+import { Container, Typography } from "@mui/material";
 import React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import fluoride from "../../../images/fluoride.png";
+import cavity from "../../../images/cavity.png";
+import whitening from "../../../images/whitening.png";
+import Service from "../Service/Service";
+
+const services = [
+  {
+    img: fluoride,
+    name: "Fluoride Treatment",
+    description:
+      "Fluoride treatments are typically professional treatments containing a high concentration of fluoride that a dentist or hygienist will apply to a persons teeth to improve health and reduce",
+  },
+  {
+    img: cavity,
+    name: "Cavity Filling",
+    description:
+      "If dental decay is caught in its early stages – before it progresses into the tooth, a filling may not be necessary. In fact, it can take four to eight years for surface decay to penetrate ",
+  },
+  {
+    img: whitening,
+    name: "Teeth Whitening",
+    description:
+      "The quickest, most dramatic way to whiten is to get an in-office bleaching at a dentist's office. Using professional-strength hydrogen peroxide and special lights or lasers, your",
+  },
+];
 
 const Services = () => {
   return (
-    <div>
-      <h2>This is services</h2>
-    </div>
+    <Box sx={{ flexGrow: 1, marginTop: 3 }}>
+      <Container>
+        <Typography
+          variant="h5"
+          sx={{ textAlign: "center", color: "#14d1ca", my: 1 }}
+        >
+          Our Services
+        </Typography>
+        <Typography
+          variant="h3"
+          sx={{ fontWeight: 500, textAlign: "center", mb: "2rem" }}
+        >
+          Services We Provide
+        </Typography>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {services.map((service) => (
+            <Service key={service.name} service={service}></Service>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
