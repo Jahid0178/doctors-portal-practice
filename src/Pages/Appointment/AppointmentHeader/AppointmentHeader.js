e72cd960e0002b595d40db0180ab88957cee03b2
Jahid <jahidulhaque767@gmail.com>
2021-11-06 20:21:35 +0600

    complete appointment header

--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,10 +1,31 @@
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import React from "react";
+import Chair from "../../../images/chair.png";
+import Calendar from "../../Shared/Calendar/Calendar";
+import bg from "../../../images/bg.png";
+
+const backgroundBg = {
+  background: `url(${bg})`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+};
 
 const AppointmentHeader = () => {
   return (
-    <Container>
-      <h2>This is appointment header</h2>
+    <Container style={backgroundBg}>
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Grid item xs={12} sm={12} md={6}>
+          <Typography variant="h4" sx={{ fontWeight: 500 }}>
+            Appointment
+          </Typography>
+          <Calendar></Calendar>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <img style={{ width: 550 }} src={Chair} alt="" />
+        </Grid>
+      </Grid>
     </Container>
   );
 };
